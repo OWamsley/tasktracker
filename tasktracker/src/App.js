a06a06da5ce5617f2dccba776c2ab0fe001be7ee
OWamsley <owamsley10@gmail.com>
2022-06-16 16:11:23 -0600

    updated packages

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/index';
 import Login from './components/login/index';
+import React from 'react';
+
 
 function App() {
   return (
